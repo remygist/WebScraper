9715b30b8579e152332904037e3b1b0c3612f8df
remygist <remy.gistelinck@student.ehb.be>
2024-10-08 14:29:20 +0200

    made a load more function

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 "use strict";
 
-async function getShoes(){
-    try{
+const shoeContainer = document.querySelector(".shoeContainer");
+const shoeList = [];
+let currentIndex = 0;
+const moreBtn = document.querySelector(".moreBtn");
+moreBtn.addEventListener("click", () => {
+    loadMoreShoes(24);
+})
+
+async function getShoes() {
+    try {
         const response = await fetch("http://localhost:3000/getShoes");
         const shoeData = await response.json();
-        const shoeContainer = document.querySelector(".shoeContainer");
-
         shoeData.forEach(shoe => {
             if (shoe.shoeImage != "" && shoe.shoeImage.startsWith("http")) {
-                shoeContainer.insertAdjacentHTML("beforeend", 
-                    `<div class="shoeItem">
-                    <img src="${shoe.shoeImage}">
-                    <h1>${shoe.shoeTitel}</h1>
-                    <p>${shoe.shoeType}</p>
-                    <p>${shoe.shoePrice}</p></div>`
-                    
-                ) 
-            }            
+                shoeList.push(shoe)
+            }
         });
-    } catch (error){
+    } catch (error) {
         console.log(error);
     }
+    console.log(shoeList);
+    loadMoreShoes(24)
+
 }
-window.onload = getShoes();
+
+function loadMoreShoes(count){
+    let itemsToLoad = Math.min(count, shoeList.length - currentIndex)
+
+    for (let i = 0; i < itemsToLoad; i++) {
+        const shoeItem = shoeList[currentIndex]
+        shoeContainer.insertAdjacentHTML("beforeend",
+            `<div class="shoeItem">
+            <img src="${shoeItem.shoeImage}">
+            <h1>${shoeItem.shoeTitel}</h1>
+            <p>${shoeItem.shoeType}</p>
+            <p>${shoeItem.shoePrice}</p></div>`
+
+        );
+        currentIndex++
+    }
+
+    itemsToLoad = Math.min(count, shoeList.length - currentIndex)
+
+    if (itemsToLoad == 0) {
+        moreBtn.style.visibility = 'hidden';
+    }
+}
+
+window.onload = getShoes;
